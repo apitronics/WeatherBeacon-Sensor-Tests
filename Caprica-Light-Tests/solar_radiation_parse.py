70b07a4845e7c06fb3e70c3d8f8cf511ba91c971
Colin Dignam <colin.p.dignam@gmail.com>
2016-03-03 22:34:58 -0800

    night

--- Caprica-Light-Tests/solar_radiation_parse.py
+++ Caprica-Light-Tests/solar_radiation_parse.py
@@ -16,7 +16,7 @@ import matplotlib
 matplotlib.style.use('ggplot')
 
 #The timezone and UTC offset break our timedate, so just ignore them
-parser = lambda date: pd.datetime.strptime(date[:-9], '%Y-%m-%d %H:%M:%S')  #2016-03-01 12:16:04
+parser = lambda date: pd.datetime.strptime(date[:-9], '%Y-%m-%d %H:%M:%S')  #2016-02-25 16:42:28 PST-0800
 
 csv_files = ['224-Cap 1.csv', '225-Cap 2.csv', '226-Cap 3.csv', '227-Cap 4.csv',
              '236-Cap 5.csv', '237-Cap 6.csv', '238-Cap 7.csv', '228-Cap 8.csv']
@@ -25,7 +25,8 @@ for csv_file in csv_files:
     #print(csv_file)
     #read csv file, index by timestamp, take only March 1st data
     df = pd.read_csv(csv_file, index_col = 'Time', 
-                     parse_dates = 'Time', date_parser=parser).loc['2016-03-01 08:00:00':]
+                     parse_dates = 'Time', date_parser=parser).loc[
+                     '2016-03-01 08:00:00':]
                     
     #find all the resets that occured
     df_restarts = df.loc[df['Status ()']>0].index
@@ -41,7 +42,7 @@ for csv_file in csv_files:
     
     
     df_am_slice['Solar Radiation (W/m²)'].plot()
-    #df_pm_slice['Solar Radiation (W/m²)'].plot()
+    df_pm_slice['Solar Radiation (W/m²)'].plot()
 
 
 plt.xlabel('Time')
