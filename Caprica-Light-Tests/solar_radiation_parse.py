c7aeca1d5d63d5ac96d624d8c5f0529803842657
Colin Dignam <colin.p.dignam@gmail.com>
2016-03-03 16:58:06 -0800

    fix tail on AM light data

--- Caprica-Light-Tests/solar_radiation_parse.py
+++ Caprica-Light-Tests/solar_radiation_parse.py
@@ -22,7 +22,7 @@ csv_files = ['224-Cap 1.csv', '225-Cap 2.csv', '226-Cap 3.csv', '227-Cap 4.csv',
              '236-Cap 5.csv', '237-Cap 6.csv', '238-Cap 7.csv', '228-Cap 8.csv']
 
 for csv_file in csv_files:
-    print(csv_file)
+    #print(csv_file)
     #read csv file, index by timestamp, take only March 1st data
     df = pd.read_csv(csv_file, index_col = 'Time', 
                      parse_dates = 'Time', date_parser=parser).loc['2016-03-01 08:00:00':]
@@ -31,15 +31,17 @@ for csv_file in csv_files:
     df_restarts = df.loc[df['Status ()']>0].index
     
     
-    #take all from start last reset
-    df_am_slice = df.loc[:df_restarts[-1]]
+    #take all from start to one before reset
+    df_am_slice = df.loc[:df_restarts[-1]].iloc[:-1]
+    
+    #print(df_am_slice['Status ()'].tail())
     
     #take all from last reset to end
     df_pm_slice = df.loc[df_restarts[-1]:]
     
     
     df_am_slice['Solar Radiation (W/m²)'].plot()
-    df_pm_slice['Solar Radiation (W/m²)'].plot()
+    #df_pm_slice['Solar Radiation (W/m²)'].plot()
 
 
 plt.xlabel('Time')
