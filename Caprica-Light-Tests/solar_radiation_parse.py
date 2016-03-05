ef247f1d8ebc4b71a55975c5ae2fb36b0718ebdb
Colin Dignam <colin.p.dignam@gmail.com>
2016-03-04 19:25:28 -0800

    write out pm solar radiation with padding

--- Caprica-Light-Tests/solar_radiation_parse.py
+++ Caprica-Light-Tests/solar_radiation_parse.py
@@ -13,7 +13,7 @@ import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-matplotlib.style.use('ggplot')
+#matplotlib.style.use('ggplot')
 
 #The timezone and UTC offset break our timedate, so just ignore them
 parser = lambda date: pd.datetime.strptime(date[:-9], '%Y-%m-%d %H:%M:%S')  #2016-02-25 16:42:28 PST-0800
@@ -21,29 +21,51 @@ parser = lambda date: pd.datetime.strptime(date[:-9], '%Y-%m-%d %H:%M:%S')  #201
 csv_files = ['224-Cap 1.csv', '225-Cap 2.csv', '226-Cap 3.csv', '227-Cap 4.csv',
              '236-Cap 5.csv', '237-Cap 6.csv', '238-Cap 7.csv', '228-Cap 8.csv']
 
-for csv_file in csv_files:
-    #print(csv_file)
+csv_file = csv_files[0]
+
+#read csv file, index by timestamp, take only March 1st data
+df = pd.read_csv(csv_file, index_col = 'Time', 
+                 parse_dates = 'Time', date_parser=parser).loc[
+                 '2016-03-01 08:00:00':]
+                 
+
+                
+#find all the resets that occured
+df_restarts = df.loc[df['Status ()']>0].index    
+#take all from start to one before reset
+df_am_slice = df.loc[:df_restarts[-1]].iloc[:-1]
+#take all from last reset to end
+df_pm_all_sensors = df.loc[df_restarts[-1]:,['Analog 3 (mV)','Solar Radiation (W/m²)']]
+
+df_pm_all_sensors = df_pm_all_sensors.rename(columns={'Solar Radiation (W/m²)':csv_file})
+
+
+for csv_file in csv_files[1:]:
     #read csv file, index by timestamp, take only March 1st data
     df = pd.read_csv(csv_file, index_col = 'Time', 
                      parse_dates = 'Time', date_parser=parser).loc[
                      '2016-03-01 08:00:00':]
                     
     #find all the resets that occured
-    df_restarts = df.loc[df['Status ()']>0].index
-    
-    
-    #take all from start to one before reset
+    df_restarts = df.loc[df['Status ()']>0].index    
+    #take all from start to one before the last reset (when we moved all the units)
     df_am_slice = df.loc[:df_restarts[-1]].iloc[:-1]
-    
-    #print(df_am_slice['Status ()'].tail())
-    
     #take all from last reset to end
-    df_pm_slice = df.loc[df_restarts[-1]:]
+    df_pm_slice = df.loc[df_restarts[-1]:,['Solar Radiation (W/m²)']]
     
     
-    df_am_slice['Solar Radiation (W/m²)'].plot()
-    df_pm_slice['Solar Radiation (W/m²)'].plot()
+     
+    df_pm_all_sensors = pd.concat([df_pm_all_sensors, df_pm_slice], axis=1)
+    
+    df_pm_all_sensors = df_pm_all_sensors.rename(columns={'Solar Radiation (W/m²)':csv_file})    
+    
+    #df_am_slice['Solar Radiation (W/m²)'].plot()
+    #df_pm_slice['Solar Radiation (W/m²)'].plot()
 
+df_pm_all_sensors = df_pm_all_sensors.fillna(method='pad')
 
+df_pm_all_sensors.plot()
 plt.xlabel('Time')
-plt.ylabel('Solar Radiation (W/m²)')
+plt.ylabel('Raw Solar Radiation (mV)')
+
+df_pm_all_sensors.to_csv('df_pm_all_sensors.csv')
